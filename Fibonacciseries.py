cfb4535c2b6ad49f9ef0144a7c95a71ea244144f
Meghanaundi <97959286+Meghanaundi@users.noreply.github.com>
2022-05-26 21:45:48 +0530

    Create Fibonacciseries.py

---
+++ Fibonacciseries.py
@@ -0,0 +1,33 @@
+nterms = int(input("Number of terms: \n"))
+n1, n2 = 0, 1
+count = 0
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
+ #output
+#Number of terms: 
+13
+#Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144 
